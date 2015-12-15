33d5b96c374e0707829d14e05850f3b1f60c4d68
Luke Opperman <luke@opperman.net>
2015-12-15 18:19:55 -0500

    Register gather/respond in constructor

--- src/Renderer.js
+++ src/Renderer.js
@@ -17,11 +17,9 @@ class Renderer {
   constructor(app) {
     this._renderers = {};
 
-    app.once('load', () => {
-      app.get('renderer').gather('register', this._registerRenderer.bind(this));
-      app.get('renderer').respond('render', this._render.bind(this));
-      app.get('renderer').respond('renderFile', this._renderFile.bind(this));
-    });
+    app.get('renderer').gather('register', this._registerRenderer.bind(this));
+    app.get('renderer').respond('render', this._render.bind(this));
+    app.get('renderer').respond('renderFile', this._renderFile.bind(this));
   }
 
   _registerRenderer (type, handler) {
